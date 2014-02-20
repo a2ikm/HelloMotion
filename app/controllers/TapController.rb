63d793f96a7910ef751b10e3f5363d8fe278f636
Masato Ikeda <masato.ikeda@gmail.com>
2014-02-20 20:24:24 +0900

    Add navigation controller

--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -10,5 +10,7 @@ class TapController < UIViewController
     @label.center = CGPointMake(self.view.frame.size.width / 2, 
                                 self.view.frame.size.height / 2)
     self.view.addSubview @label
+
+    self.title = "Tap"
   end
 end
