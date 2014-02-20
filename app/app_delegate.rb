06ab71f79d30bd79b343824a22188d7aba9bebaf
Masato Ikeda <masato.ikeda@gmail.com>
2014-02-20 20:07:57 +0900

    Add view to window

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,6 +3,10 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
+
     true
   end
 end
